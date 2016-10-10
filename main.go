190757c8e892189fef630dde0c451b40336f489a
Bogdan Radacina <bogdan.radacina@invoice2go.com>
2016-10-10 16:50:07 +1100

    Added interrupt handling

--- main.go
+++ main.go
@@ -3,8 +3,16 @@ package main
 import (
 	"log"
 	"os"
+	"os/signal"
 	"path"
+	"runtime/pprof"
 )
+
+var logFile *os.File
+
+func closeLog() {
+	logFile.Close()
+}
 
 func initLog(folder string) {
 	err := os.MkdirAll(folder, os.ModeDir)
@@ -14,17 +22,42 @@ func initLog(folder string) {
 
 	log.SetFlags(log.LstdFlags)
 	logPath := path.Join(folder, "gomp3.log")
-	f, err := os.Create(logPath)
+	logFile, err = os.Create(logPath)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	log.SetOutput(f)
+	log.SetOutput(logFile)
+}
+
+func watchForInterrupts() {
+	interrupt := make(chan os.Signal, 100)
+	signal.Notify(interrupt, os.Interrupt, os.Kill)
+
+	for _ = range interrupt {
+		pprof.StopCPUProfile()
+		closeLog()
+		os.Exit(1)
+	}
 }
 
 func main() {
 	c := getConfig()
 	initLog(c.LogPath)
+	defer closeLog()
+
+	profPath := path.Join(c.LogPath, "gomp3.prof")
+	f, err := os.Create(profPath)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f.Close()
+
+	pprof.StartCPUProfile(f)
+	defer pprof.StopCPUProfile()
+
+	go watchForInterrupts()
+
 	p := NewPlayer(c.Mp3Location)
 
 	StartWeb(p, c.WebappPath, c.Port)
